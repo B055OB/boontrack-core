25e0799c8b0336237cd3eabcf4f158bb602f75aa
BO55OB <onlineboost.id@gmail.com>
2026-08-13 19:41:44 +0700

    Fix import path for payment and add analytics blueprint

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from routes.payment import payment_bp
+from app.routes.payment import payment_bp
+from app.routes.analytics_route import analytics_bp
 
 app = Flask(__name__)
 
-# Register Blueprint Webhook
+# Register Blueprint Webhook & Analytics
 app.register_blueprint(payment_bp)
+app.register_blueprint(analytics_bp)
 
 if __name__ == '__main__':
     print("Server BoonTrack Listener Berjalan di Port 5000...")
